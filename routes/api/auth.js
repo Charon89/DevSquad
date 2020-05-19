728425ad0669572616b06202178f8cba83d487bf
Dmytro Andriushchenko <46615932+Charon89@users.noreply.github.com>
2020-05-19 00:04:46 -0400

    Added: User auth, profile APIs, User APIs

---
+++ routes/api/auth.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../../middleware/auth');
+const User = require('../../models/User');
+
+const {check, validationResult} = require('express-validator');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const config = require('config');
+
+// @route   GET api/auth
+// @desc    Test route
+// @access  Public
+
+router.get('/', auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        await res.json(user);
+    } catch (e) {
+        console.log(e.message);
+        res.status(500).send('Server error')
+    }
+});
+
+// @route   POST api/auth
+// @desc    Authenticate User and get token
+// @access  Public
+
+router.post('/', [
+        check('email', 'Please include an email').isEmail(),
+        check('password', 'Please enter a password with 6 or more characters').exists()
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({errors: errors.array()});
+        }
+
+        const {email, password} = req.body;
+
+        try {
+            let user = await User.findOne({email});
+            // See if user exists
+            if (!user) {
+                return res.status(400).json({errors: [{msg: 'Invalid credentials'}]});
+            }
+
+            const isMatch = await bcrypt.compare(password, user.password);
+
+            if (!isMatch) {
+                return res.status(400).json({errors: [{msg: 'Invalid credentials'}]});
+            }
+
+            // Return JWT
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            };
+            jwt.sign(
+                payload,
+                config.get('jwtSecret'),
+                {expiresIn: 360000},
+                (err, token) => {
+                    if (err) throw err;
+                    console.log(token);
+                    res.json({token});
+                })
+        } catch (e) {
+            console.log(e.error);
+            return res.status(500).send('Authentication server error')
+        }
+
+    }
+)
+;
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
+const connectDB = require('./config/db');
+
+// Connect database
+connectDB();
+
+// Init Middleware
+app.use(express.json({extended: false}))
 
 app.get('/', (req, res) => {
     res.send('Hello')
-})
+});
 
-app.listen(PORT, () => console.log(`Server is started and running on port: ${PORT}`))
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
+app.listen(PORT, () => console.log(`Server is started and running on port: ${PORT}`));

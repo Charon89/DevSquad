6e83932d1f81015401e66edb65afb7c11de1078d
Dmytro Andriushchenko <46615932+Charon89@users.noreply.github.com>
2020-07-03 00:29:15 -0400

    Prepared for deployment

--- server.js
+++ server.js
@@ -2,20 +2,27 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 const connectDB = require('./config/db');
+const path = require('path');
 
 // Connect database
 connectDB();
 
 // Init Middleware
-app.use(express.json({extended: false}))
-
-app.get('/', (req, res) => {
-    res.send('DevSquad API...')
-});
+app.use(express.json({extended: false}));
 
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 app.listen(PORT, () => console.log(`Server is started and running on port: ${PORT}`));
